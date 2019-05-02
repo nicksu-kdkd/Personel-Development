c77b4d8364a8e6ef938918f85f033db141a8171c
Nick Su <nicksu383@gmail.com>
2019-05-02 23:51:20 +0800

    update confluence

--- confluence-ci.py
+++ confluence-ci.py
@@ -4,14 +4,8 @@
 
 import requests, json
 import sys
-
-LINK = "http://127.0.0.1:8090/rest/api/content"
-USER = "nick"
-PASSWORD = "123456"
-option = sys.argv[1]
-TITLE = sys.argv[2]
-
-index = {'content': TITLE}
+import argparse
+import markdown
 
 # query the page id for parent 
 def query_id(TITLE):
@@ -25,19 +19,21 @@ def query_id(TITLE):
 	return ID
 
 # post doc
-def post_doc(TITLE):
+def post_doc(TITLE, index):
 	headers = {
 	    'Content-Type': 'application/json',
 	}
+
+	content = to_html(TITLE)
 
 	parent = [k for k, v in index.items() if TITLE in v]
 
 	if parent:
 		parent_id = query_id(parent[0])
-		data = {'type':'page','title':TITLE,'ancestors':[{'type':'page','id':parent_id}],'space':{'key':'DEV'},'body':{'storage':{'value': 'content','representation':'storage'}}}
+		data = {'type':'page','title':TITLE,'ancestors':[{'type':'page','id':parent_id}],'space':{'key':'DEV'},'body':{'storage':{'value': content,'representation':'storage'}}}
 	else:
-		data = {'type':'page','title':TITLE,'space':{'key':'DEV'},'body':{'storage':{'value': 'content','representation':'storage'}}}
-
+		data = {'type':'page','title':TITLE,'space':{'key':'DEV'},'body':{'storage':{'value': content,'representation':'storage'}}}
+	print(data)
 	response = requests.post(LINK, auth=(USER, PASSWORD), data=json.dumps(data), headers=headers)
 
 	if response.status_code == requests.codes.ok:
@@ -47,6 +43,30 @@ def post_doc(TITLE):
 		print(response.text)
 		sys.exit(1)
 
+# transform markdown to xhtml
+def to_html(TITLE):
+	ori = open(TITLE, 'r')
+	md = ori.read().decode('utf8')
+	html = markdown.markdown(md, output_format='xhtml')
+	print(html)
+	return(html)
+	
 
 if __name__=="__main__":
-	post_doc(TITLE)
+	
+	parser = argparse.ArgumentParser(description='manual to this script')
+	parser.add_argument('--link', type=str, default="http://127.0.0.1:8090/rest/api/content")
+	parser.add_argument('--user', type=str, default="nick")
+	parser.add_argument('--passwd', type=str, default=123456)
+	parser.add_argument('--title', type=str, default=None)
+
+	args = parser.parse_args()
+	LINK = args.link
+	USER = args.user
+	PASSWORD = args.passwd
+	TITLE = args.title
+
+	print(LINK, USER, PASSWORD, TITLE)
+	
+	index = {'content': TITLE}
+	post_doc(TITLE, index)
